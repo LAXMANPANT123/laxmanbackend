1f2cfadfa9104d17e5fc8e58f14c952453b05d2f
LAXMANPANT123 <laxmanpant32@gmail.com>
2025-04-17 16:11:46 +0530

    adding the files

---
+++ models/user.model.js
@@ -0,0 +1,53 @@
+models
+
+
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,                  //what type of data will come from in the database
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        dafault: "user",
+    },
+    isverified: {
+        type: Boolean,
+        dafault: false,
+    },
+    verificationToken: {
+        type: String,
+    },
+    resetPasswordToken: {
+        type: String,
+    },
+    resetPasswordExpires: {
+        type: Date,
+    },
+}, 
+{
+    timestamps: true,
+}
+);
+
+userSchema.pre("save", async function(){
+   if(this.isModified("password")){
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+}
+else{
+
+}
+
+
+
+});
+
+
+
+const User = mongoose.model("User", userSchema)
+
+export default User;
